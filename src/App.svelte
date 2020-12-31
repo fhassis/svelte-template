fffaa1807163079896aef4a0829992c0a14f7d4c
fhassis <fhassis2@gmail.com>
2020-12-31 17:56:43 -0300

    Added bulma classes to test.

--- src/App.svelte
+++ src/App.svelte
@@ -3,28 +3,12 @@
 </script>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	p {
+		color:chocolate;
 	}
 </style>
 
-<main>
-	<h1>Hello {name}!</h1>
+<main class="container has-text-centered">
+	<h1 class="is-size-1">Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
